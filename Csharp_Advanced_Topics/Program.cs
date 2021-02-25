59305585a6bd9ec953af1364908ac02a0004167a
Mike-Kohl <kohl.mike@gmail.com>
2021-02-25 15:48:08 -0500

    Updated notes

--- Csharp_Advanced_Topics/Program.cs
+++ Csharp_Advanced_Topics/Program.cs
@@ -1,8 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System.Numerics;
+using System.Windows;
+using System;
 
 namespace Csharp_Advanced_Topics
 {
@@ -10,6 +9,33 @@ namespace Csharp_Advanced_Topics
     {
         static void Main(string[] args)
         {
+            VectorSamples();
+        }
+
+        private static void VectorSamples() 
+        {
+            Vector1();
+        }
+
+        private static void Vector1() 
+        {
+            byte[] array1 = Enumerable.Range(1, 128)
+                .Select(x => (byte)x).ToArray();
+            byte[] array2 = Enumerable.Range(4, 128)
+                .Select(x => (byte)x).ToArray();
+            byte[] result = new byte[128];
+
+            int size = Vector<byte>.Count;
+
+            for (int i = 0; i < array1.Length; i += size)
+            {
+                var va = new Vector<byte>(array1, i);
+                var vb = new Vector<byte>(array2, i);
+                var vresult = va + vb;
+                vresult.CopyTo(result, i);
+            }
+
+        
         }
     }
 }
